4ad01dcfba3c4c1ea0a4eb4788f6d1d63b37c9bc
Brett Morris <brettmorris21@gmail.com>
2016-01-17 15:46:29 -0800

    Fixing cpu params for hyak

--- hat11.py
+++ hat11.py
@@ -32,7 +32,7 @@ whole_lc = LightCurve.from_raw_fits(light_curve_paths, name='HAT11')
 transits = LightCurve(**whole_lc.mask_out_of_transit(hat11_params)
                       ).get_transit_light_curves(hat11_params)
 
-transit_numbers = range(60, 70)
+transit_numbers = [62]
 
 for transit_number in transit_numbers:
     lc = transits[transit_number]
@@ -46,13 +46,15 @@ for transit_number in transit_numbers:
     best_fit_spot_params = peak_finder(lc.times.jd, residuals, lc.errors,
                                        hat11_params, n_peaks=4, plots=False,
                                        verbose=True)
-    best_fit_gaussian_model = summed_gaussians(lc.times.jd, best_fit_spot_params)
+    best_fit_gaussian_model = summed_gaussians(lc.times.jd,
+                                               best_fit_spot_params)
 
     # If spots are detected:
     if best_fit_spot_params is not None:
         output_path = os.path.join(output_dir,
                                    'chains{0:03d}.hdf5'.format(transit_number))
         sampler = run_emcee_seeded(lc, hat11_params, best_fit_spot_params,
-                                   n_steps=500, n_walkers=40, n_threads=8,
-                                   output_path=output_path, burnin=0.0, n_extra_spots=1)
+                                   n_steps=15000, n_walkers=200, n_threads=32,
+                                   output_path=output_path, burnin=0.5,
+                                   n_extra_spots=1)
 
