ed9cc7dd5d6ee7e21cf5f658a73f185e4990425a
Brett Morris <brettmorris21@gmail.com>
2016-02-18 11:25:11 -0800

    K17 prep

--- k17/k17_hyak_launch.py
+++ k17/k17_hyak_launch.py
@@ -2,9 +2,9 @@
 import sys
 sys.path.insert(0, '../')
 
-from friedrich.hyak import launch_hyak_run
+from friedrich.hyak2 import launch_hyak_run
 
-launch_hyak_run(n_transits=583,
+launch_hyak_run(n_transits=1, #583,
                 run_script='/usr/lusers/bmmorris/git/friedrich/k17/k17_hyak.py',
                 run_dir='/usr/lusers/bmmorris/git/friedrich/k17',
                 job_name='friedrich_k17')
