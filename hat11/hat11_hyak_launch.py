42b9aa26db02581965c7d6fe72719a02135cb3a8
Brett Morris <brettmorris21@gmail.com>
2016-06-02 15:17:31 -0700

    correcting hyak launcher for proper path

--- hat11/hat11_hyak_launch.py
+++ hat11/hat11_hyak_launch.py
@@ -4,7 +4,8 @@ sys.path.insert(0, '../')
 
 from friedrich.hyak import launch_hyak_run
 
-launch_hyak_run(n_transits=205, 
+launch_hyak_run(n_transits=205,
+                output_dir='/gscratch/stf/bmmorris/friedrich/hat11/'
                 job_name='friedrich_hat11',
                 run_dir='/usr/lusers/bmmorris/git/friedrich/hat11',
                 run_script='/usr/lusers/bmmorris/git/friedrich/hat11/hat11_hyak.py')
