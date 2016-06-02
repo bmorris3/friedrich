46c22d95558916506f98d845e65f5ea8ffacf740
Brett Morris <brettmorris21@gmail.com>
2016-06-02 15:14:18 -0700

    specify output path

--- hat11_rm_soln_2/hat11_hyak_launch.py
+++ hat11_rm_soln_2/hat11_hyak_launch.py
@@ -5,6 +5,7 @@ sys.path.insert(0, '../')
 from friedrich.hyak import launch_hyak_run
 
 launch_hyak_run(n_transits=205, 
-                job_name='friedrich_hat11',
+                output_dir='/gscratch/stf/bmmorris/friedrich/hat11_rm_soln_2/',
+                job_name='friedrich_hat11_rm_soln_2',
                 run_dir='/usr/lusers/bmmorris/git/friedrich/hat11_rm_soln_2',
                 run_script='/usr/lusers/bmmorris/git/friedrich/hat11_rm_soln_2/hat11_hyak.py')
