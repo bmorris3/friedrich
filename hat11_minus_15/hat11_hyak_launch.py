6c5bd7dea91a369ed8582734d7a4a99f6552bc91
Brett Morris <brettmorris21@gmail.com>
2016-06-14 10:30:32 -0700

    small test run

--- hat11_minus_15/hat11_hyak_launch.py
+++ hat11_minus_15/hat11_hyak_launch.py
@@ -4,7 +4,7 @@ sys.path.insert(0, '../')
 
 from friedrich.hyak import launch_hyak_run
 
-launch_hyak_run(n_transits=205,
+launch_hyak_run(n_transits=4,
                 job_name='friedrich_hat11_minus_15',
                 output_dir='/gscratch/stf/bmmorris/friedrich/hat11_minus_15/',
                 run_dir='/usr/lusers/bmmorris/git/friedrich/hat11_minus_15/',
