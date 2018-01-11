b154ddfeca4859340b549335dc399f7296044f1b
Brett Morris <brettmorris21@gmail.com>
2018-01-10 18:14:26 -0800

    update

--- hat11/hat11_hyak_arctic_launch.py
+++ hat11/hat11_hyak_arctic_launch.py
@@ -5,7 +5,7 @@ sys.path.insert(0, '../')
 from friedrich.hyak import launch_hyak_run
 
 launch_hyak_run(n_transits=1,
-                output_dir='/gscratch/stf/bmmorris/friedrich/hat11/',
+                output_dir='/gscratch/stf/bmmorris/friedrich/hat11/arctic/',
                 job_name='friedrich_hat11',
                 run_dir='/usr/lusers/bmmorris/git/friedrich/hat11',
                 run_script='/usr/lusers/bmmorris/git/friedrich/hat11/hat11_arctic.py')
